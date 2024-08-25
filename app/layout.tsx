a0ad67ef00ad3d18b6681133737707806f686a1d
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-25 19:27:09 -0400

    font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Calistoga, Inter } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+	subsets: ["latin"],
+	variable: "--font-serif",
+	weight: "400",
+});
 
 export const metadata: Metadata = {
 	title: "Spotify Shuffler",
@@ -17,7 +23,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body
+				className={twMerge(
+					inter.variable,
+					calistoga.variable,
+					"bg-black text-white antialiased font-sans"
+				)}
+			>
+				{children}
+			</body>
 		</html>
 	);
 }
