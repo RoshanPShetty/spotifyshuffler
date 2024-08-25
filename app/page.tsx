37a6174e1679554d3a20f45a6ef9203ca5370975
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-25 19:28:11 -0400

    Added all sections to landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
+import Contact from "@/components/Contact";
+import Reviews from "@/components/Reviews";
 import Hero from "@/components/Hero";
+import Header from "@/components/Header";
+import Features from "@/components/Features";
+import Countdown from "@/components/CountDown";
 
 export default function Home() {
 	return (
 		<div>
+			<Header />
 			<Hero />
+			<Countdown />
+			<Features />
+			<Reviews />
+			<Contact />
 		</div>
 	);
 }
