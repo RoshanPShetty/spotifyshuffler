52ccefb51d9f20a8871e18361b61601ae7d51830
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-25 19:26:59 -0400

    Added data

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,22 @@
+export const header = [
+	{
+		title: "Home",
+		path: "#home",
+	},
+	{
+		title: "Features",
+		path: "#features",
+	},
+	{
+		title: "Reviews",
+		path: "#reviews",
+	},
+	{
+		title: "Contact",
+		path: "#contact",
+	},
+];
+
 export const socials = [
 	{
 		id: 1,
@@ -18,5 +37,42 @@ export const socials = [
 		id: 4,
 		name: "Privacy",
 		link: "/privacy-policy",
+	},
+];
+
+export const reviews = [
+	{
+		profilePic:
+			"https://lh3.googleusercontent.com/a/ACg8ocLLAlrgZivuRKuHphPPPEl5eRSjLBz7U4cvFqkO8VoUb6exEA=s48-w48-h48",
+		username: "Lumina",
+		rating: "5 out of 5 stars",
+		date: "Aug 19, 2024",
+		reviewText:
+			"Fantastic! It skips intros and episodes effortlessly, making binge-watching a breeze. Super easy to use and it just works—love it!",
+	},
+	{
+		profilePic:
+			"https://lh3.googleusercontent.com/a-/ALV-UjUgYC8VHvrKx4XxeBefPSy4qyZL7OItHKyJpaxhA4TkLMVz2w1m=s48-w48-h48",
+		username: "Rishita Shetty",
+		rating: "5 out of 5 stars",
+		date: "Aug 20, 2024",
+		reviewText:
+			"I really like the extension, it works really well. Hope you make more of these for other apps!",
+	},
+	{
+		profilePic:
+			"https://lh3.googleusercontent.com/a-/ALV-UjWffiHY4nG88Qo4DdzJZOK3zaAYd-VmhtrYFt3Dkk6QLKM_Bes=s48-w48-h48",
+		username: "Akemi Jin",
+		rating: "5 out of 5 stars",
+		date: "Aug 19, 2024",
+		reviewText: "Please create another one for Peacock and Hulu!",
+	},
+	{
+		profilePic:
+			"https://lh3.googleusercontent.com/a-/ALV-UjVYLJpfapRYQVPXxlNvaDez9nqN5zVk4bqI28aLNGUpM8EpERs=s48-w48-h48",
+		username: "Archie Mannu",
+		rating: "5 out of 5 stars",
+		date: "Aug 19, 2024",
+		reviewText: "Great Extension! Simple but very effective",
 	},
 ];
