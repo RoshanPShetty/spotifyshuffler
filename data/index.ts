f6484d2e9992c1d486366db7c9cdc3dd6334bd4e
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-26 04:17:32 -0400

    updated reviews data; revised album images

--- data/index.ts
+++ data/index.ts
@@ -43,6 +43,15 @@ export const socials = [
 export const reviews = [
 	{
 		profilePic:
+			"https://lh3.googleusercontent.com/a/ACg8ocLLAlrgZivuRKuHphPPPEl5eRSjLBz7U4cvFqkO8VoUb6exEA=s48-w48-h48",
+		username: "Lumina",
+		rating: "5 out of 5 stars",
+		date: "Aug 19, 2024",
+		reviewText:
+			"Fantastic! It skips intros and episodes effortlessly, making binge-watching a breeze. Super easy to use and it just works—love it!",
+	},
+	{
+		profilePic:
 			"https://lh3.googleusercontent.com/a-/ALV-UjX9ZoJ3U3koMEzC10adZB8vrfFqh_8DFVUgIuBMr5eHef7eHZDp=s48-w48-h48",
 		username: "Navya s",
 		rating: "5 out of 5 stars",
@@ -52,12 +61,11 @@ export const reviews = [
 	},
 	{
 		profilePic:
-			"https://lh3.googleusercontent.com/a/ACg8ocLLAlrgZivuRKuHphPPPEl5eRSjLBz7U4cvFqkO8VoUb6exEA=s48-w48-h48",
-		username: "Lumina",
+			"https://lh3.googleusercontent.com/a-/ALV-UjWffiHY4nG88Qo4DdzJZOK3zaAYd-VmhtrYFt3Dkk6QLKM_Bes=s48-w48-h48",
+		username: "Akemi Jin",
 		rating: "5 out of 5 stars",
 		date: "Aug 19, 2024",
-		reviewText:
-			"Fantastic! It skips intros and episodes effortlessly, making binge-watching a breeze. Super easy to use and it just works—love it!",
+		reviewText: "Please create another one for Peacock and Hulu!",
 	},
 	{
 		profilePic:
@@ -70,18 +78,77 @@ export const reviews = [
 	},
 	{
 		profilePic:
-			"https://lh3.googleusercontent.com/a-/ALV-UjWffiHY4nG88Qo4DdzJZOK3zaAYd-VmhtrYFt3Dkk6QLKM_Bes=s48-w48-h48",
-		username: "Akemi Jin",
-		rating: "5 out of 5 stars",
-		date: "Aug 19, 2024",
-		reviewText: "Please create another one for Peacock and Hulu!",
-	},
-	{
-		profilePic:
 			"https://lh3.googleusercontent.com/a-/ALV-UjVYLJpfapRYQVPXxlNvaDez9nqN5zVk4bqI28aLNGUpM8EpERs=s48-w48-h48",
 		username: "Archie Mannu",
 		rating: "5 out of 5 stars",
 		date: "Aug 19, 2024",
 		reviewText: "Great Extension! Simple but very effective",
+	},
+];
+
+export const albums = [
+	{
+		id: 1,
+		src: "https://kprofiles.com/wp-content/uploads/2023/06/YOASOBI_-_Idol.webp",
+	},
+	{
+		id: 2,
+		src: "https://i.scdn.co/image/ab67616d0000b273a9f9b6f07b43009f5b0216dc",
+	},
+	{
+		id: 3,
+		src: "https://spaces.rocksound.tv/uploads/2023/08/02.jpg",
+	},
+	{
+		id: 4,
+		src: "https://i.scdn.co/image/ab67616d0000b2730c47f3a81c00ec342eccd03f",
+	},
+	{
+		id: 5,
+		src: "https://i.ebayimg.com/images/g/6skAAOSwEDlksb3E/s-l1200.jpg",
+	},
+	{
+		id: 6,
+		src: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png",
+	},
+	{
+		id: 7,
+		src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX4mxbhjAhylUPED9JvE4mM53NkkfADZ9JNg&s",
+	},
+	{
+		id: 8,
+		src: "https://m.media-amazon.com/images/I/71S6QJ9n9yL._UF1000,1000_QL80_.jpg",
+	},
+	{
+		id: 9,
+		src: "https://www.udiscovermusic.com/wp-content/uploads/2017/08/Pink-Floyd-Dark-Side-Of-The-Moon.jpg",
+	},
+	{
+		id: 10,
+		src: "https://www.skillshare.com/blog/wp-content/uploads/2020/10/AwakenMyLoveE28093Childish.jpeg",
+	},
+	{
+		id: 11,
+		src: "https://www.rollingstone.com/wp-content/uploads/2024/06/Tyler-the-Creator-Igor.jpg?w=800",
+	},
+	{
+		id: 12,
+		src: "https://www.billboard.com/wp-content/uploads/2023/07/rihanna-anti-cover-2016-billboard-1240.jpg?w=600",
+	},
+	{
+		id: 13,
+		src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoLKsPoSjTpzoVGjy566w6KtCQ3cLyqf_4Sg&s",
+	},
+	{
+		id: 14,
+		src: "https://www.udiscovermusic.com/wp-content/uploads/2017/08/NWA-Straight-Outta-Compton.jpg",
+	},
+	{
+		id: 15,
+		src: "https://www.billboard.com/wp-content/uploads/2022/03/42.-Nicki-Minaj-%E2%80%98The-Pinkprint-2014-album-art-billboard-1240.jpg?w=600",
+	},
+	{
+		id: 16,
+		src: "https://www.printmag.com/wp-content/uploads/2021/06/fdcd5a_d8dd6d540bd84e4e9df8cbcfa376ce0dmv2.jpg",
 	},
 ];
